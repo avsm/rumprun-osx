a9bb23c669c272f1c2f42a21aea1e0b8aa03a893
Anil Madhavapeddy <anil@recoil.org>
2015-11-02 14:42:57 +0000

    Add first cut at a one-stop MacOS X rumprun build script

---
+++ README.md
@@ -0,0 +1,9 @@
+Rump Kernel on OSX
+==================
+
+This repository provides an installation script that builds the [Rump Kernel](http://rumpkernel.org) toolchain on MacOSX.
+
+Instructions were derived from:
+- <https://github.com/rumpkernel/wiki/wiki/Howto:-Set-up-a-Rumprun-toolchain-in-Mac-OS-X>
+- <https://github.com/rumpkernel/wiki/wiki/Tutorial%3A-Building-Rumprun-Unikernels>
+
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -ex
+
+DESTDIR=${DESTDIR:-`pwd`}
+CROSSENV="${DESTDIR}/cross-env.sh"
+RRENV="${DESTDIR}/rr-env.sh"
+cd ${DESTDIR}
+
+if [ ! -d netbsd ]; then
+  git clone -b netbsd-7 https://github.com/IIJ-NetBSD/netbsd-src.git netbsd
+elif [ -d netbsd/.git ]; then
+  git -C netbsd pull -u
+fi
+
+cd netbsd
+./build.sh -U -T elftools.x86_64 -m amd64 tools
+
+echo export PATH=\"$PWD/elftools.x86_64/bin:\$PATH\" > ${CROSSENV}
+echo export READELF=\"$PWD/elftools.x86_64/bin/x86_64--netbsd-readelf\" >> ${CROSSENV}
+echo export CC=\"x86_64--netbsd-gcc\" >> ${CROSSENV}
+chmod a+x ${CROSSENV}
+
+cd ${DESTDIR}
+eval `cat ${CROSSENV}`
+
+if [ ! -d rumprun ]; then
+  git clone http://repo.rumpkernel.org/rumprun
+  git -C rumprun submodule update --init
+elif [ -d rumprun/.git ]; then
+  git -C rumprun pull -u
+  # TODO something with submodules here?
+fi
+
+cd rumprun
+./build-rr.sh hw
+
+echo export PATH=\"$DESTDIR/rumprun/rumprun/bin:\$PATH\" > ${RRENV}
+chmod a+x ${RRENV}
